236389d751b10c5fabe81369f3756448d5218f9b
huynhdanhlang <danhlangbmvl@gmail.com>
2021-08-25 20:39:45 +0700

    add base login and sigup UI

--- fontend/app/src/App.js
+++ fontend/app/src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Login from "./componets/Login";
+import Register from "./componets/Register";
+import Home from "./componets/Home";
+import Profile from "./componets/Profile";
+import BoardUser from "./componets/BoardUser";
+import BoardAuthor from "./componets/BoardAuthor";
+import BoardModerator from "./componets/BoardModerator";
+import BoardAdmin from "./componets/BoardAdmin";
+
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+
+import { history } from "./helpers/history";
+
+const App = () => {
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAuthorBoard, setShowAuthorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+
+  const { user: currentUser } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen((location) => {
+      dispatch(clearMessage()); //clear message when changing location
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (currentUser) {
+      setShowAdminBoard(currentUser.roles.includes("ROLE_MODERATOR"));
+      setShowModeratorBoard(currentUser.roles.includes("ROLE_ADMIN"));
+      setShowAuthorBoard(currentUser.roles.includes("ROLE_AUTHOR"));
+    }
+  }, [currentUser]);
+
+  const logOut = () => {
+    dispatch(logout());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            danhlang
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav">
+              <li className="nav-item active">
+                <Link to={"/home"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+              {showAdminBoard && (
+                <li className="nav-item">
+                  <Link to={"/admin"} className="nav-link">
+                    Admin
+                  </Link>
+                </li>
+              )}
+              {showModeratorBoard && (
+                <li className="nav-item">
+                  <Link to={"/moderator"} className="nav-link">
+                    Moderator
+                  </Link>
+                </li>
+              )}
+              {showAuthorBoard && (
+                <li className="nav-item">
+                  <Link to={"/author"} className="nav-link">
+                    Author
+                  </Link>
+                </li>
+              )}
+              {currentUser && (
+                <li className="nav-item">
+                  <Link to={"/user"} className="nav-link">
+                    User
+                  </Link>
+                </li>
+              )}
+            </div>
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">{currentUser.username}</Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={logOut}>
+                  Logout
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link" >Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">Signup</Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container-fluid !direction !spacing">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+            <Route path="/author" component={BoardAuthor} />
+            <Route path="/mod" component={BoardModerator} />
+            <Route path="/admin" component={BoardAdmin} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ fontend/app/src/componets/Register.js
@@ -0,0 +1,166 @@
+import React, { useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { isEmail } from "validator";
+import { register } from "../actions/auth";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Trường bắt buộc.Vui lòng nhập!
+      </div>
+    );
+  }
+};
+
+const validEmail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Email không hợp lệ.Vui lòng nhập lại!
+      </div>
+    );
+  }
+};
+
+const validusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Tên người dùng phải từ 3 đên 25 ký tự
+      </div>
+    );
+  }
+};
+
+const validpassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Mật khẩu phải từ 6 đến 40 ký tự
+      </div>
+    );
+  }
+};
+
+const Register = () => {
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [successful, setSuccessful] = useState(false);
+
+  const { message } = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const handleResgister = (e) => {
+    e.preventDefault();
+
+    setSuccessful(false);
+
+    form.current.validateAll();
+
+    if (checkBtn.current.context._errors.length === 0) {
+      dispatch(register(username, email, password))
+        .then(() => {
+          setSuccessful(true);
+        })
+        .catch(() => {
+          setSuccessful(false);
+        });
+    }
+  };
+
+  return (
+    <div className="col-md-12">
+      <div className="card card-container">
+        <img
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          alt="profile-img"
+          className="profile-img-card"
+        />
+
+        <Form onSubmit={handleResgister} ref={form}>
+          {!successful && (
+            <div>
+              <div className="form-group">
+                <label htmlFor="username">Tên người dùng</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={onChangeUsername}
+                  validations={[required, validusername]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Mật khẩu</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="password"
+                  value={password}
+                  onChange={onChangePassword}
+                  validations={[required, validpassword]}
+                />
+              </div>
+              &nbsp;
+              <div className="form-group">
+                <button type="button" className="btn btn-primary">
+                  Đăng ký
+                </button>
+              </div>
+            </div>
+          )}
+          {message && (
+            <div className="form-group">
+              <div
+                className={
+                  successful ? "alert alert-success" : "alert alert-danger"
+                }
+                role="alert"
+              >
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: "none" }} ref={checkBtn} />
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
